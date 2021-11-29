37ef42daf1f647a782f3a8d90d78a7bcbdc47f5b
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-29 17:24:39 +0100

    add loading

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,19 @@
 <template>
     <main class="container mt-3">
-        <cardProp 
-            v-for="(movie, index) in arrayMovies" :key="`${index}`"
-            :ArrayMovies="movie"
-            :title="movie.title"
-            :originalTitle="movie.original_title"
-            :lang="movie.original_language"
-            :vote="movie.vote_average"
-        />
+        <div v-if="arrayMovies.length > 0">
+            <cardProp
+                v-for="(movie, index) in arrayMovies" :key="`${index}`"
+                :ArrayMovies="movie"
+                :title="movie.title"
+                :originalTitle="movie.original_title"
+                :lang="movie.original_language"
+                :vote="movie.vote_average"
+            />
+        </div>
+        <div v-else class="loading">
+            <h1>no results found</h1>
+            <i class="load-icon fas fa-circle-notch my-5"></i>
+        </div>
     </main>
 </template>
 
@@ -28,4 +34,19 @@ export default {
 <style scoped lang="scss">
 @import '@/style/utilities';
 
+    .loading{
+        h1{
+            font-size: 50px;
+        }
+        .load-icon{
+            font-size: 50px;
+            animation: rotation 1.2s linear infinite;
+            
+            @keyframes rotation {
+                0%{
+                    transform: rotate(-360deg);
+                }
+            }
+        }
+    }
 </style>
