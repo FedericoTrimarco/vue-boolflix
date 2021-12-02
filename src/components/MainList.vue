70d841b490a63d974a11f9b30f05557584097bc3
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-02 15:50:08 +0100

    remove scrollbar & add scrollLeft() e scrollRight()

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -1,7 +1,10 @@
 <template>
-    <section class="border d-flex flex-wrap">
+<section class="d-flex">
+
+    <i class="align-self-center me-1 fas fa-chevron-circle-left pointer" v-show="arrayList.length > 5" @click="scrollRight"></i>
+    <div class="cards-list border d-flex" ref="list">
         <cardProp
-            class="ms-1"
+            class="mx-1"
             v-for="el in arrayList" :key="el.id"
             :image="el.poster_path || el.poster_path"
             :title="el.title || el.name"
@@ -10,7 +13,9 @@
             :vote="el.vote_average || el.vote_average"
             :plot="el.overview"
         />
-    </section>
+    </div>
+    <i class="fas fa-chevron-circle-right align-self-center pointer ms-1" v-show="arrayList.length > 5" @click="scrollLeft"></i>
+</section>
 </template>
 
 <script>
@@ -22,11 +27,35 @@ export default {
     },
     props:{
         arrayList: Array,
+    },
+    methods: {
+        scrollLeft(){
+            this.$refs.list.scrollLeft += 2000;
+        },
+        scrollRight(){
+            this.$refs.list.scrollLeft -= 2000;
+        }
     }
+
 }
 
 </script>
 
 <style scoped lang="scss">
 @import '@/style/utilities';
+@import '@/style/variables';
+.cards-list{
+    overflow-x: scroll;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+}
+i{
+    font-size: 50px;
+    color: rgb(211, 211, 211, 0.15);
+    transition: color .5s;
+    &:hover{
+        color: $primary-color;
+    }
+}
 </style>
