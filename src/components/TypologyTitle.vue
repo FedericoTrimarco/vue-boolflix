490dccae16662361fd62dcc52c098ccf1641dd63
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-02 19:07:25 +0100

    add TypologyTitle

---
+++ src/components/TypologyTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="tv-movie-list d-flex justify-content-center align-items-center my-4">
+        <img src="../assets/small-logo.png" alt="small-logo">
+        <h1 class="text-white ms-1" v-show="mainArray.length !== 0">{{text}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TypologyTitle',
+    props: {
+        mainArray: Array,
+        text: String,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/style/utilities';
+.tv-movie-list{
+  text-align: center;
+  margin: 50px 0;
+  letter-spacing: 5px;
+}
+</style>
