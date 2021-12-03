d4b500baeb953e2fbea11c16f45c7a181db55fc0
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-03 02:59:04 +0100

    add scrollRight(), scrollLeft(), returnOriginalBgColor()

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -1,29 +1,31 @@
 <template>
-<section class="d-flex">
+<section class="d-flex border main-list" @mouseleave="returnOriginalBgColor">
+              <!-- arrow-left -->
     <i 
         class="align-self-center me-1 fas fa-chevron-circle-left pointer" 
         v-show="arrayList.length > 5" 
-        @click="scrollRight"
+        @click="scrollLeft"
     >
     </i>
-
+              <!-- cards-list -->
     <div class="cards-list border d-flex" ref="list">
         <cardProp
             class="mx-1"
             v-for="el in arrayList" :key="el.id"
-            :image="el.poster_path || el.poster_path"
+            :backgroundImage="el.backdrop_path"
+            :image="el.poster_path"
             :title="el.title || el.name"
             :originalTitle="el.original_title || el.original_name"
-            :lang="el.original_language || el.original_language"
-            :vote="el.vote_average || el.vote_average"
+            :lang="el.original_language"
+            :vote="el.vote_average"
             :plot="el.overview"
         />
     </div>
-
+              <!-- arrow-right -->
     <i 
         class="fas fa-chevron-circle-right align-self-center pointer ms-1" 
         v-show="arrayList.length > 5" 
-        @click="scrollLeft"
+        @click="scrollRight"
     >
     </i>
 </section>
@@ -40,11 +42,14 @@ export default {
         arrayList: Array,
     },
     methods: {
-        scrollLeft(){
+        scrollRight(){
             this.$refs.list.scrollLeft += 1200;
         },
-        scrollRight(){
+        scrollLeft(){
             this.$refs.list.scrollLeft -= 1200;
+        },
+        returnOriginalBgColor(){
+            document.querySelector('#app').style.backgroundImage = "url('https://www.icolorpalette.com/download/solidcolorimage/141414_solid_color_background_icolorpalette.png')";
         }
     }
 
@@ -55,18 +60,21 @@ export default {
 <style scoped lang="scss">
 @import '@/style/utilities';
 @import '@/style/variables';
-.cards-list{
-    overflow-x: scroll;
-    &::-webkit-scrollbar {
-        display: none;
+.main-list{
+    .cards-list{
+        overflow-x: scroll;
+        &::-webkit-scrollbar {
+            display: none;
+        }
     }
-}
-i{
-    font-size: 50px;
-    color: rgb(211, 211, 211, 0.15);
-    transition: color .5s;
-    &:hover{
-        color: $primary-color;
+    i{
+        font-size: 50px;
+        color: rgba(211, 211, 211, 0.582);
+        transition: color .5s;
+        &:hover{
+            color: $primary-color;
+        }
     }
+
 }
 </style>
